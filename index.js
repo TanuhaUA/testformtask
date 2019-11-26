a862b1837cc7ef59918b8ccf634a9008868cc407
tanuha <tanuha@zeoalliance.com>
2019-11-26 07:23:30 +0200

    add generated password

---
+++ index.js
@@ -0,0 +1,16 @@
+window.onload = function() {
+    function makePass(length) {
+        var result = '';
+        var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        var charactersLength = characters.length;
+        for ( var i = 0; i < length; i++ ) {
+            result += characters.charAt(Math.floor(Math.random() * charactersLength));
+        }
+        return result;
+    }
+
+    var passField = document.getElementById('id4');
+    if (passField) {
+        passField.value = makePass(6);
+    }
+};
